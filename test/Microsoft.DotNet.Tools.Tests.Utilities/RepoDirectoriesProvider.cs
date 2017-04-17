8488a3141c7173236579dcdacc5ad3133ab822e3
William Li <wul@microsoft.com>
2017-04-17 14:09:31 -0700

    Remove test base dependencies

--- test/Microsoft.DotNet.Tools.Tests.Utilities/RepoDirectoriesProvider.cs
+++ test/Microsoft.DotNet.Tools.Tests.Utilities/RepoDirectoriesProvider.cs
@@ -4,24 +4,27 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Xml.Linq;
 using Microsoft.DotNet.PlatformAbstractions;
-using Microsoft.DotNet.Tools.Tests.Utilities;
 
 namespace Microsoft.DotNet.Tools.Test.Utilities
 {
     public class RepoDirectoriesProvider
     {
+        public RepoDirectoriesProvider(
+            string artifacts = null,
+            string nugetPackages = null,
+            string pjDotnet = null)
+        {
+            Artifacts = artifacts ?? Path.Combine(RepoRoot, "artifacts", BuildRid);
+            NugetPackages = nugetPackages ?? Path.Combine(RepoRoot, ".nuget", "packages");
+            PjDotnet = pjDotnet ?? GetPjDotnetPath();
+        }
+
         private static string s_repoRoot;
         private static string s_buildRid;
-
-        private string _artifacts;
-        private string _builtDotnet;
-        private string _nugetPackages;
-        private string _stage2Sdk;
-        private string _stage2WithBackwardsCompatibleRuntimesDirectory;
-        private string _testPackages;
-        private string _pjDotnet;
+        public string Artifacts { get; }
+        public string NugetPackages { get; }
+        public string PjDotnet { get; }
 
         public static string RepoRoot
         {
@@ -35,20 +38,14 @@ namespace Microsoft.DotNet.Tools.Test.Utilities
 #if NET451
                 string directory = AppDomain.CurrentDomain.BaseDirectory;
 #else
-                string directory = AppContext.BaseDirectory;
+                var directory = AppContext.BaseDirectory;
 #endif
-
                 while (!Directory.Exists(Path.Combine(directory, ".git")) && directory != null)
                 {
                     directory = Directory.GetParent(directory).FullName;
                 }
 
-                if (directory == null)
-                {
-                    throw new Exception("Cannot find the git repository root");
-                }
-
-                s_repoRoot = directory;
+                s_repoRoot = directory ?? throw new Exception("Cannot find the git repository root");
                 return s_repoRoot;
             }
         }
@@ -57,50 +54,19 @@ namespace Microsoft.DotNet.Tools.Test.Utilities
         {
             get
             {
+                if (!string.IsNullOrEmpty(s_buildRid)) return s_buildRid;
+                s_buildRid = RuntimeEnvironment.GetRuntimeIdentifier();
                 if (string.IsNullOrEmpty(s_buildRid))
                 {
-                    s_buildRid = RuntimeEnvironment.GetRuntimeIdentifier();
-
-                    if (string.IsNullOrEmpty(s_buildRid))
-                    {
-                        throw new InvalidOperationException($"Could not find a property named 'Rid'");
-                    }
+                    throw new InvalidOperationException($"Could not find a property named 'Rid'");
                 }
-                
                 return s_buildRid;
             }
-        }
-
-        public string Artifacts => _artifacts;
-        public string BuiltDotnet => _builtDotnet;
-        public string NugetPackages => _nugetPackages;
-        public string PjDotnet => _pjDotnet;
-        public string Stage2Sdk => _stage2Sdk;
-        public string Stage2WithBackwardsCompatibleRuntimesDirectory => _stage2WithBackwardsCompatibleRuntimesDirectory;
-        public string TestPackages => _testPackages;
-
-        public RepoDirectoriesProvider(
-            string artifacts = null,
-            string builtDotnet = null,
-            string nugetPackages = null,
-            string corehostPackages = null,
-            string corehostDummyPackages = null,
-            string pjDotnet = null)
-        {
-            _artifacts = artifacts ?? Path.Combine(RepoRoot, "artifacts", BuildRid);
-            _builtDotnet = builtDotnet ?? Path.Combine(_artifacts, "intermediate", "sharedFrameworkPublish");
-            _nugetPackages = nugetPackages ?? Path.Combine(RepoRoot, ".nuget", "packages");
-            _pjDotnet = pjDotnet ?? GetPjDotnetPath();
-            _stage2Sdk = Directory.EnumerateDirectories(Path.Combine(_artifacts, "stage2", "sdk")).First();
-            _stage2WithBackwardsCompatibleRuntimesDirectory =
-                Path.Combine(_artifacts, "stage2WithBackwardsCompatibleRuntimes");
-            _testPackages = Path.Combine(RepoRoot, "artifacts", "testpackages", "packages");
         }
 
         private string GetPjDotnetPath()
         {
             return new DirectoryInfo(Path.Combine(RepoRoot, ".dotnet_stage0PJ"))
-                .GetDirectories().First()
                 .GetFiles("dotnet*").First()
                 .FullName;
         }
